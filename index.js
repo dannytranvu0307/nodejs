934adc7b84f7e46a4ab658ab70f555621ec43699
vu tran <quachvotinh006@gmail.com>
2022-03-06 17:25:57 +0900

    adada

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express')
+const morgan = require('morgan');
+const path = require('path');
 const app = express()
+const port = 3000
+const {create} = require("express-handlebars")
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+    //--------------------------------------------// app.use(morgan('combined'));
+  app.use(express.static(path.join(__dirname,'src/public')));
+
+
+   const hbs = create({ extname: '.hbs'})
+   app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+    app.set("views", path.join(__dirname, 'src/resources/views'));
+    
+    app.get('/', (req, res) => {
+        res.render('home');
+    });
+    app.get('/new', (req, res) => {
+        res.render('new');
+    });
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 })
-
-app.listen(3000)
